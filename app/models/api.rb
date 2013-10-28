cc8af1825ebb7e2a5abbf9ba593b348b233895bc
Sara LaFassett <sara@lafassett.com>
2013-10-27 18:12:47 -0700

    Adding validation to forms and models for required fields

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,4 +1,8 @@
 class Api < ActiveRecord::Base
   attr_accessible :name, :url, :description, :requires_key
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
+  validates :requires_key, presence: true
   has_and_belongs_to_many :mashups
 end
--- app/models/mashup.rb
+++ app/models/mashup.rb
@@ -1,4 +1,7 @@
 class Mashup < ActiveRecord::Base
   attr_accessible :name, :url, :description
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
   has_and_belongs_to_many :apis
 end
