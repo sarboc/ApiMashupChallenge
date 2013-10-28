0a0ffc018baeba9215327109b1b3c9aa0a3fef0c
Sara LaFassett <sara@lafassett.com>
2013-10-27 18:54:48 -0700

    Adding validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+  before_save :create_remember_token
+
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+	def create_remember_token
+		self.remember_token = self.email + SecureRandom.urlsafe_base64
+  end
 end
